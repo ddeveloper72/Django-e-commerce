55d3082ccfc17d7c5ae08c79f24db7d1f27d496d
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-05 21:47:07 +0000

    Added the Product clsss to the admin.py in the products app, so products is accessible from the admin site.

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Product
 
 # Create your tests here.
+
+class ProductTest(TestCase):
+    """Here we will define the tests
+    that we will run against oour Post models
+    """
+
+    def test_str(self):
+        test_name = Product(name='Some product')
+        self.assertEqual(str(test_name), 'Some product')
