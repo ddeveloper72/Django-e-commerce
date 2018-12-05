16a333f08749c6bc7e2a52d4bbc7c5c3f00f620f
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-05 21:56:41 +0000

    Installed Pillow. Made migrations & migrated products app to  create products database. Updated requirements.txt

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_legnt=254, default='')
+    name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
