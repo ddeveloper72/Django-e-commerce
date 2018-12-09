675888a60a28104bc264ea31b2b6913eee0007c0
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-09 13:42:00 +0000

    Started work on debugging add to cart not working.

--- cart/views.py
+++ cart/views.py
@@ -9,9 +9,10 @@ def view_cart(request):
 
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
-
+    
     quantity=int(request.POST.get('quantity'))
-
+    print('quantity', quantity)
+    
     cart = request.session.get('cart', {})
     cart[id] = cart.get(id, quantity)
 
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = (os.path.join(BASE_DIR, "media"))
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/' 
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
