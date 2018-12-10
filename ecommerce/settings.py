4d2fdacb77ddc237dd0cd569c1027d860f93f85d
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-10 13:45:31 +0000

    Installed Stripe and setup environmental variables.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,6 +143,9 @@ MEDIA_ROOT = (os.path.join(BASE_DIR, "media"))
 
 
 MEDIA_URL = '/media/' 
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
