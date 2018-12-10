f111e743e0b9fc977e0fb069d3e1df15599fdf82
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-10 22:10:18 +0000

    Added the checkout  models to the admin.py making them accessible to the admin page.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+
+class OrderLineAdminInline(admin.TabularInline):
+    """
+    TabularInline subclass defines the template used to render
+    the Order in the admin interface. StackInline is another
+    option.
+    """
+    model = OrderLineItem
+
+class OrderAdmin(admin.ModelAdmin):
+    """
+    The admin interface has the ability to edit more that one
+    model on a single page.  This is known as inlines.
+    """
+    inlines = (OrderLineAdminInline, )
+    
+admin.site.register(Order, OrderAdmin)
+
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'accounts',
     'products',
     'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
