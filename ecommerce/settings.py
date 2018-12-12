5de154faa546b3fae71622eed1bfc319431653a5
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-12 13:48:25 +0000

    Created migrations and setup comented out configurations for the production database.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -14,6 +14,11 @@ import os
 # import env
 import dj_database_url
 
+""" if os.environ.get('DEVELOPMENT'):
+    development = True
+else:
+    development = False """
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -27,7 +32,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = [
+    os.environ.get('ddeveloper72-ecommerce'),
     os.environ.get('C9_HOSTNAME'),
     os.environ.get('localhost', '127.0.0.1')
 ]
@@ -85,13 +92,17 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+""" if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))} """
 
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
