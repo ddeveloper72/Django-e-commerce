51fbbef61a4f38a960020c11ee1bb194a7e5fd61
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-12 13:16:17 +0000

    Added password reset facility by email.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -150,4 +150,10 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
