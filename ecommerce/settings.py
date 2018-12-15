ba2b000fb73d872e6fedbd7ab0f5b61b73f4a89d
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-15 22:11:01 +0000

    Reversed development boolean

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -18,9 +18,9 @@ import dj_database_url
 
 
 if os.environ.get('DEVELOPMENT'):
-    development = False
-else:
     development = True
+else:
+    development = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
