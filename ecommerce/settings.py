2abad102b83cf0f194e46d48bcf002f6660880cf
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-15 21:06:11 +0000

    Fixed switch between development and production database selection.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -16,10 +16,10 @@ import os
 
 import dj_database_url
 
-""" if os.environ.get('DEVELOPMENT'):
+if os.environ.get('DEVELOPMENT', 'True'):
     development = True
 else:
-    development = False """
+    development = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
 
 ALLOWED_HOSTS = [
@@ -94,10 +94,7 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if "DATABASE_URL" in os.environ:
-    print("Development environ not detected. DATABASE_URL")
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
-else:
+if development:
     print("Development environ detected. Using SQLite instead")
     DATABASES =  {
         'default': {
@@ -105,6 +102,9 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+else:
+    print("Development environ not detected. Using remote DATABASE_URL")
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
