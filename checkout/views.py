b34a491d62a54b876121021c52d9ddda2246a1be
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-15 19:25:54 +0000

    Removed os environ switch to align with tutorial.

--- checkout/views.py
+++ checkout/views.py
@@ -38,7 +38,7 @@ def checkout(request):
 
             try: 
                 customer = stripe.Charge.create(
-                    ammount = int(total * 100),
+                    amount = int(total * 100),
                     currency = "EUR",
                     description = request.user.email,
                     card = payment_form.cleaned_data['stripe_id'],
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -16,11 +16,6 @@ import env
 
 import dj_database_url
 
-""" if os.environ.get('DEVELOPMENT'):
-    development = True
-else:
-    development = False """
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
