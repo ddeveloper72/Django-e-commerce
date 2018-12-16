d421f4cc49c328e3f4f1d36cd3c4213337b47e38
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-16 22:26:28 +0000

    Added a missing / to r'^checkout'

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),  
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
-    url(r'^checkout', include(urls_checkout)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT})
 ]
