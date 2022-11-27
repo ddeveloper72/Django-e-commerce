efc924b11f3678584a685eb3a579bed2b6af59f6
ddeveloper72 <duncan.niamh@gmail.com>
2022-11-27 21:28:57 +0000

    added AWS static files storage locations

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -170,8 +170,11 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+AWS_LOCATION = 'static'
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     )
